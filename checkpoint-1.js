fbf1d419fa6b35cbf1fafaccc738ff17a1e54a56
huyphungtiny <huyphung@tinypulse.com>
2018-07-16 13:02:43 +0700

    Re-submit the checkpoint 1

--- checkpoint-1.js
+++ checkpoint-1.js
@@ -1,10 +1,1 @@
-// The conplexity of the following code is base on the variable arr
-
-// Case sorted ascending array like [1, 2, 3, 4, 5]
-// The number of loop times will be n => O(N)
-
-// Case sorted descending array like [5, 4, 3, 2, 1]
-// The number of loop times will be (n*(n+1))/2 => O(N^2)
-
-// When the array is random (unsort) we cannot calculate the complexity for
-// that general case
+// Option D => O(N) because j is not reset to zero when i start in new loop
---
+++ checkpoint-2.js
@@ -0,0 +1,8 @@
+// Print concentric rectangular pattern in a 2d matrix.
+// Let us show you some examples to clarify what we mean.
+
+function concentric(num) {
+  for (let index = 0; index < num; index++) {
+    
+  }
+}
